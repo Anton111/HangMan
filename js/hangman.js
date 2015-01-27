753a3bfb3706eb05cf114da886f46a9e0dcf1b35
anton111 <aanastasia111@gmail.com>
2015-01-27 10:43:46 -0500

    Rough layout of the game

---
+++ js/hangman.js
@@ -0,0 +1,68 @@
+(function(){
+    
+    var app = angular.module('hangman', []);
+    
+    app.controller('HangmanController', function () {
+        
+        this.hangmanWord = '';
+        //this.wordArray = ['a', 'b', 'c', 'd', 'e'];
+        //this.wordArray = [];
+        this.image = 'images/hangman.jpg';
+        this.hintOption = '';
+        this.definition = '';
+        this.setupComplete = false;
+        
+    });
+    
+    app.controller('SetupController', function(){
+        this.done = function(hangman){
+            hangman.wordArray =[];
+            
+            for(var i = 0; i <hangman.hangmanWord.length; i++)
+            {
+                hangman.wordArray.push(i);         
+            }
+            
+            hangman.setupComplete = true;
+        };    
+    });
+    
+        
+    app.controller('DataEntryController', function(){
+        
+        this.submit = function(hangman){
+            
+        };
+    });
+    
+    app.controller('HintController',['$http', function($http){
+        this.getHint = function(hangman){
+            
+            if(typeof(hangman.hintOption !== 'undefined')){
+                if(hangman.hintOption === 'Standard Definition'){
+                    var standardDictUrl = 'https://montanaflynn-dictionary.p.mashape.com/define?word={0}';
+                    standardDictUrl = standardDictUrl.replace('{0}', hangman.hangmanWord);
+                    
+                    var standardDictConfig = {headers: { 'X-Mashape-Key': '6mu2zVKmZ1mshiwmLQaz9njkStapp1NGD01jsnRSo50kJjCQg0'}};
+                    
+                    $http.get(standardDictUrl, standardDictConfig).success(function(data){
+                        hangman.definition = data.definitions[0].text;
+                    });
+                }
+                else if(hangman.hintOption === 'Urban Dictionary Definition'){
+                    var urbanDictUrl = 'https://mashape-community-urban-dictionary.p.mashape.com/define?term={0}';
+                    urbanDictUrl = urbanDictUrl.replace('{0}', hangman.hangmanWord);
+                    
+                    var urbanDictConfig = {headers: { 'X-Mashape-Key': '6mu2zVKmZ1mshiwmLQaz9njkStapp1NGD01jsnRSo50kJjCQg0'}};
+                    
+                    $http.get(urbanDictUrl, urbanDictConfig).success(function(data){
+                        hangman.definition = data.list[0].definition;
+                    });
+                }
+            }
+            
+        };
+    }]);
+    
+})();
+ 
